4f9defcc49e28a9ae00f792eec479fe220b3017c
rosePoetry <107481802+rosePoetry@users.noreply.github.com>
2022-08-24 16:10:14 +0300

    Add files via upload

---
+++ Calculator.h
@@ -0,0 +1,26 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#endif // CALCULATOR_H
+
+
+// example class of test
+class Calculator
+{
+public:
+    Calculator(int a = 0, int b = 0) : mA(a), mB(b) { }
+
+    int GetA() const { return mA; }
+    void SetA(int a) { mA = a; }
+    int GetB() const { return mB; }
+    void SetB(int b) { mB = b; }
+
+    int Sum() const { return mA + mB; }
+    int Diff() const { return mA - mB; }
+    int Mult() const { return mA * mB; }
+    int Div() const { return mA / mB; }
+
+private:
+    int mA;
+    int mB;
+};
---
+++ testCalculator.h
@@ -0,0 +1,28 @@
+#ifndef TESTCALCULATOR_H
+#define TESTCALCULATOR_H
+
+#endif // TESTCALCULATOR_H
+
+#include "Calculator.h"
+
+#include <QtTest>
+
+class TestCalculator: public QObject
+{
+    Q_OBJECT
+
+private slots:
+    // -- setup/cleanup --
+    void init();
+
+    // -- tests --
+    void testConstructor();
+    void testSum();
+
+private:
+    const int A0 = 0;
+    const int B0 = 0;
+
+private:
+    Calculator mCalc;
+};
